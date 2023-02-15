e82bd7f2fc7749a90b2053bd223195a3f2e5ab08
Natan Souza <natan.souza@guaranisistemas.com.br>
2023-02-15 16:03:41 -0300

    first commit with firebase configuration

---
+++ android/app/src/main/kotlin/com/example/curious_ifsp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.curious_ifsp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'dart:io';
+
+import 'package:curious_ifsp/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Inicialize o Firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    name: "curious_ifsp",
+    options: Platform.isLinux
+        ? DefaultFirebaseOptions.web
+        : DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Inicie o aplicativo Flutter
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // ignore: prefer_typing_uninitialized_variables
+  late final databaseReference;
+  late String value;
+
+  // final ref = databaseReference.ref("room/123");
+
+  // await ref.set({
+  //   "name": "John",
+  //   "age": 18,
+  //   "address": {"line1": "100 Mountain View"}
+  // });
+  @override
+  void initState() {
+    super.initState();
+    final databaseReference = FirebaseDatabase.instance;
+    init(databaseReference);
+    get(databaseReference);
+  }
+
+  init(FirebaseDatabase database) async {
+    await database.ref("/rooms/123").set("Olá mundo");
+  }
+
+  get(FirebaseDatabase database) async {
+    database.ref("/rooms/123").onValue.listen((event) {
+      final data = event.snapshot.value;
+      setState(() {
+        value = data.toString();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(child: Center(child: Text(value))),
+      ),
+    );
+  }
+}
